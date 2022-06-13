75f3c71493eae9807c826a4ddd0a2b4d6c9d9787
agsuvidha <agsuvidha@gmail.com>
2022-06-13 23:00:00 +0530

    added

--- storage.py
+++ storage.py
@@ -2,7 +2,7 @@ import csv
 
 all_movies = []
 
-with open('final.csv') as f:
+with open('final.csv',encoding='utf-8') as f:
     reader = csv.reader(f)
     data = list(reader)
     all_movies = data[1:]
